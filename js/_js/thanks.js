4ab89d07760cc0e21c6933a30b123b62815d9921
pavelp21 <pavelvp2182@yandex.ru>
2019-11-06 16:55:52 +0500

    Bonus, Home tasks, Promo, Thanks and Remind pages are done

---
+++ js/_js/thanks.js
@@ -0,0 +1,125 @@
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend intlTelInput.min.js
+//@prepros-prepend utils.js
+
+var isIE = false || !!document.documentMode;
+
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "../css/thanks-ie.min.css";
+    head.appendChild(link);
+}
+
+$(document).ready(function () {
+    $('.input-mail').intlTelInput({
+        defaultCountry: "ru",
+        initialCountry: "auto",
+        preferredCountries: ["ru", "ua", 'az', 'am', 'by', 'kz', 'kg', 'md', 'tj', 'uz', 'tm', 'ge'],
+        autoPlaceholder: 'aggressive',
+        nationalMode: false,
+        customPlaceholder: function (selectedCountryPlaceholder, selectedCountryData) {
+            return "+" + selectedCountryData.dialCode;
+        },
+        geoIpLookup: function (success, failure) {
+            /*
+            $.get( "https://ip-api.com/json/", function( data ) {
+                var countryCode = (data.countryCode) ? data.countryCode : "ru";
+                success(countryCode);
+            }, "json" );*/
+
+            $.get("https://ipinfo.io", function () {}, "jsonp").always(function (resp) {
+                var countryCode = (resp && resp.country) ? resp.country : "ru";
+                success(countryCode);
+            });
+        },
+        separateDialCode: false,
+        formatOnDisplay: false,
+        utilsScript: 'https://mk.beauty-matrix.ru/assets/plugins/intltelinput/js/utils.js',
+    });
+    $(function () {
+        var e = new Date,
+            t = e.getDate(),
+            n = $(".switch-date");
+        switch (t) {
+            case 5:
+                n.text("NN числа");
+                break;
+            case 6:
+                n.text("NN числа");
+                break;
+            default:
+                n.text("NN числа")
+        }
+    });
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this);
+        email.keypress(function (e) {
+            if ((e.keyCode < 48 || e.keyCode > 57) && 43 != e.keyCode) {
+                this.value += '';
+                message.text('Только цифры!').slideDown(500);
+                return false
+            } else {
+                message.slideUp(500);
+            }
+        });
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                reNone = /.+/,
+                message = $('.alert-message', this),
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+
+            if (!email.val().match(reNone)) {
+                email.css({
+                    "borderColor": "red",
+                    'transition': 'all .4s ease'
+                });
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            email.css({
+                "borderColor": "#0a1938",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#0a1938",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#0a1938",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+});
