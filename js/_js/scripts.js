28757821c218b26804c6e261f38b9f8935ff2477
pavelp21 <pavelvp2182@yandex.ru>
2019-11-07 17:47:04 +0500

    Main page is adapted

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -2,39 +2,49 @@
 //@prepros-prepend bootstrap.min.js
 //@prepros-prepend jquery.fancybox.min.js
 
+var isIE = false || !!document.documentMode;
+
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
+
 $(document).ready(function () {
     $(function () {
         var check = $('.check', this),
             email = $('.input-mail', this),
-            message = $('.alert-message', this),
-            button = $('.button-modal', this),
-            switch_title = $('.switch-title'),
-            switch_text = $('.switch-text'),
-            currDate = new Date(),
-            startDate = new Array(
-                new Date("November 04 2019 18:30 UTC+3"),
-                // new Date("November 05 2019 18:30 UTC+3"),
-                // new Date("November 05 2019 18:30 UTC+3"),
-                new Date("November 06 2019 18:30 UTC+3")),
-            endDate = new Array(
-                new Date("November 04 2019 21:00 UTC+3"),
-                // new Date("November 05 2019 21:00 UTC+3"),
-                // new Date("November 05 2019 21:00 UTC+3"),
-                new Date("November 06 2019 21:00 UTC+3")),
-            rooms = new Array(
-                'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d1',
-                'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d2',
-                'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d2',
-                'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d3');
+            message = $('.alert-message', this);
+        //     button = $('.button-modal', this),
+        //     switch_title = $('.switch-title'),
+        //     switch_text = $('.switch-text'),
+        //     currDate = new Date(),
+        //     startDate = new Array(
+        //         new Date("November 04 2019 18:30 UTC+3"),
+        //         // new Date("November 05 2019 18:30 UTC+3"),
+        //         // new Date("November 05 2019 18:30 UTC+3"),
+        //         new Date("November 06 2019 18:30 UTC+3")),
+        //     endDate = new Array(
+        //         new Date("November 04 2019 21:00 UTC+3"),
+        //         // new Date("November 05 2019 21:00 UTC+3"),
+        //         // new Date("November 05 2019 21:00 UTC+3"),
+        //         new Date("November 06 2019 21:00 UTC+3")),
+        //     rooms = new Array(
+        //         'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d1',
+        //         'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d2',
+        //         'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d2',
+        //         'https://start.bizon365.ru/room/18087/Transpersonal_analytics_d3');
 
-        function roomSwitch() {
-            switch_title.removeClass('hide').html('А Вы записались на БЕСПЛАТНЫЙ ВЕБИНАР?<br>ЖДЕМ ТОЛЬКО ВАС!');
-            switch_text.removeClass('hide').html('<span class="hurryup">Скорее ЗАХОДИТЕ прямо СЕЙЧАС на вебинар!</span><br>Присоединяйтесь к интенсиву и окунитесь в мир практической философии! Познайте этот мир и себя в нём!');
-            button.text('Зайти!');
-        }
-        if ((currDate > startDate[0] && currDate < endDate[0]) || (currDate > startDate[1] && currDate < endDate[1]) || (currDate > startDate[2] && currDate < endDate[2]) || (currDate > startDate[3] && currDate < endDate[3])) {
-            roomSwitch();
-        }
+        // function roomSwitch() {
+        //     switch_title.removeClass('hide').html('А Вы записались на БЕСПЛАТНЫЙ ВЕБИНАР?<br>ЖДЕМ ТОЛЬКО ВАС!');
+        //     switch_text.removeClass('hide').html('<span class="hurryup">Скорее ЗАХОДИТЕ прямо СЕЙЧАС на вебинар!</span><br>Присоединяйтесь к интенсиву и окунитесь в мир практической философии! Познайте этот мир и себя в нём!');
+        //     button.text('Зайти!');
+        // }
+        // if ((currDate > startDate[0] && currDate < endDate[0]) || (currDate > startDate[1] && currDate < endDate[1]) || (currDate > startDate[2] && currDate < endDate[2]) || (currDate > startDate[3] && currDate < endDate[3])) {
+        //     roomSwitch();
+        // }
         $(".form").on("submit", function () {
             var check = $('.check', this),
                 message = $('.alert-message', this),
@@ -62,18 +72,18 @@ $(document).ready(function () {
                 return false;
             }
             if (email.val() && check) {
-                if (currDate > startDate[0] && currDate < endDate[0]) {
-                    window.open(rooms[0]);
-                }
-                if (currDate > startDate[1] && currDate < endDate[1]) {
-                    window.open(rooms[1]);
-                }
-                if (currDate > startDate[2] && currDate < endDate[2]) {
-                    window.open(rooms[2]);
-                }
-                if (currDate > startDate[3] && currDate < endDate[3]) {
-                    window.open(rooms[3]);
-                }
+                // if (currDate > startDate[0] && currDate < endDate[0]) {
+                //     window.open(rooms[0]);
+                // }
+                // if (currDate > startDate[1] && currDate < endDate[1]) {
+                //     window.open(rooms[1]);
+                // }
+                // if (currDate > startDate[2] && currDate < endDate[2]) {
+                //     window.open(rooms[2]);
+                // }
+                // if (currDate > startDate[3] && currDate < endDate[3]) {
+                //     window.open(rooms[3]);
+                // }
                 button.text('Отправляем...');
                 setTimeout(function () {
                     button.text('Отправлено!');
